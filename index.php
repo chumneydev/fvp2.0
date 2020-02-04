42f2dc83fcb611f7084445096de7ee72b22ff0c4
Louis Stephens <lstephens@chumneyads.com>
2020-02-04 13:45:02 -0500

    index added

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Florida Video Post</title>
+    <link rel="stylesheet" href="dist/css/fvp.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+
+    <!-- login -->
+    <div id="login" data-aos="flip-left" data-aos-duration="800">
+
+        <img src="dist/images/logo.png" class="logo" />
+
+        <form action="#">
+            <input type="text" name="username" id="username" placeholder="Username" />
+            <input type="password" name="password" id="password" placeholder="Password" />
+
+            <!--<button class="submit" type="submit" value="Submit"><i class="fab fa-telegram-plane"></i> Submit</button>-->
+            <a class="submit" href="#"><i class="fab fa-telegram-plane"></i> Submit</a>
+
+        </form>
+
+        <p>Forgot your password? <a href="#">Click here</a> to have support reach out to you.</p>
+
+
+    </div> <!-- login -->
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script src="dist/js/all.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script type="text/javascript">
+        AOS.init();
+    </script>
+
+</body>
+
+</html>
--- simple/dashboard.php
+++ simple/dashboard.php
@@ -41,7 +41,7 @@
 
             <!-- login -->
             <div id="add-client" data-aos="flip-left" data-aos-duration="800">
-                <form action="#">
+                <!--<form action="#">
 
                     <div class="autoselect">
                         <input type="text" name="clients" id="clients" placeholder="Start typing the client code..." />
@@ -50,25 +50,24 @@
                     <input type="text" name="job" id="job" placeholder="Job Number" />
                     <input type="text" name="title" id="title" placeholder="Title" />
                     <input type="text" name="duration" id="duration" placeholder="Duration" />
-                    <input type="file" name="video" id="video" placeholder="File" />
-
-                    <!--<button class="submit" type="submit" value="Submit"><i class="fab fa-telegram-plane"></i> Submit</button>-->
+                    <input type="file" name="video" id="video" class="file-input" placeholder="Choose a Video" />
+                    <label for="video">Choose a Video</label>
                     <a class="submit" href="#"><i class="fab fa-telegram-plane"></i> Add Client</a>
 
                 </form>
+            </div> -->
+            <!-- login -->
+
+
+ 
+
+        <iframe src=""></iframe>
+        <img src=""/>
 
 
 
-            </div> <!-- login -->
 
-
-
-
-
-
-
-
-        </div>
+            </div>
         <!-- content -->
 
 
@@ -97,119 +96,79 @@
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
     <script src="dist/js/all.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+    <script src="https://apis.google.com/js/api.js"></script>
+    
+    <script>
+
+        var key = 'AIzaSyBuNb7uDwoGqbeFk6uBtO4QFelg2KdhOYU';
+        var channelId = 'UCzX5VuQFrT49lV1Vvdup_xg';
+        var client = 'zX5VuQFrT49lV1Vvdup_xg'; 
+
+        axios.get('https://www.googleapis.com/youtube/v3/playlists', {
+            params: {
+                /*part: 'snippet',
+                maxResults: 1,
+                key: key,
+                q: 'CHMO',*/
+                key: 'AIzaSyBuNb7uDwoGqbeFk6uBtO4QFelg2KdhOYU',
+                channelId: 'UCzX5VuQFrT49lV1Vvdup_xg',
+                q: "CHMO",
+                part: 'snippet',
+                maxResults: 20,
+                type: 'video',
+                videoEmbeddable: true,
+            }
+        })
+        
+        .then(function (data) {
+            //embedVideo(data);
+            console.log (data)
+        })
+  
+        .catch(function (error) {
+            console.log(error);
+        })
+  
+  
+  
+        function embedVideo(data) {
+            $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id)
+            //$('h3').text(data.items.snippet.title)
+            //$('.description').text(data.items.snippet.description)
+            $('img').attr('src', data.items[0].snippet.thumbnails.high.url)
+        }
+
+
+  
+  
+    </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <script type="text/javascript">
         AOS.init();
-
-        var clients = ['BSTO', 'BSKS', 'CMED', 'ROFL', 'VOFM'];
-        autocomplete(document.getElementById("clients"), clients);
-
-
-
-
-
-        function autocomplete(inp, arr) {
-            /*the autocomplete function takes two arguments,
-            the text field element and an array of possible autocompleted values:*/
-            var currentFocus;
-            /*execute a function when someone writes in the text field:*/
-            inp.addEventListener("input", function(e) {
-                var a, b, i, val = this.value;
-                /*close any already open lists of autocompleted values*/
-                closeAllLists();
-                if (!val) {
-                    return false;
-                }
-                currentFocus = -1;
-                /*create a DIV element that will contain the items (values):*/
-                a = document.createElement("DIV");
-                a.setAttribute("id", this.id + "autocomplete-list");
-                a.setAttribute("class", "autocomplete-items");
-                /*append the DIV element as a child of the autocomplete container:*/
-                this.parentNode.appendChild(a);
-                /*for each item in the array...*/
-                for (i = 0; i < arr.length; i++) {
-                    /*check if the item starts with the same letters as the text field value:*/
-                    if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                        /*create a DIV element for each matching element:*/
-                        b = document.createElement("DIV");
-                        /*make the matching letters bold:*/
-                        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                        b.innerHTML += arr[i].substr(val.length);
-                        /*insert a input field that will hold the current array item's value:*/
-                        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                        /*execute a function when someone clicks on the item value (DIV element):*/
-                        b.addEventListener("click", function(e) {
-                            /*insert the value for the autocomplete text field:*/
-                            inp.value = this.getElementsByTagName("input")[0].value;
-                            /*close the list of autocompleted values,
-                            (or any other open lists of autocompleted values:*/
-                            closeAllLists();
-                        });
-                        a.appendChild(b);
-                    }
-                }
-            });
-            /*execute a function presses a key on the keyboard:*/
-            inp.addEventListener("keydown", function(e) {
-                var x = document.getElementById(this.id + "autocomplete-list");
-                if (x) x = x.getElementsByTagName("div");
-                if (e.keyCode == 40) {
-                    /*If the arrow DOWN key is pressed,
-                    increase the currentFocus variable:*/
-                    currentFocus++;
-                    /*and and make the current item more visible:*/
-                    addActive(x);
-                } else if (e.keyCode == 38) { //up
-                    /*If the arrow UP key is pressed,
-                    decrease the currentFocus variable:*/
-                    currentFocus--;
-                    /*and and make the current item more visible:*/
-                    addActive(x);
-                } else if (e.keyCode == 13) {
-                    /*If the ENTER key is pressed, prevent the form from being submitted,*/
-                    e.preventDefault();
-                    if (currentFocus > -1) {
-                        /*and simulate a click on the "active" item:*/
-                        if (x) x[currentFocus].click();
-                    }
-                }
-            });
-
-            function addActive(x) {
-                /*a function to classify an item as "active":*/
-                if (!x) return false;
-                /*start by removing the "active" class on all items:*/
-                removeActive(x);
-                if (currentFocus >= x.length) currentFocus = 0;
-                if (currentFocus < 0) currentFocus = (x.length - 1);
-                /*add class "autocomplete-active":*/
-                x[currentFocus].classList.add("autocomplete-active");
-            }
-
-            function removeActive(x) {
-                /*a function to remove the "active" class from all autocomplete items:*/
-                for (var i = 0; i < x.length; i++) {
-                    x[i].classList.remove("autocomplete-active");
-                }
-            }
-
-            function closeAllLists(elmnt) {
-                /*close all autocomplete lists in the document,
-                except the one passed as an argument:*/
-                var x = document.getElementsByClassName("autocomplete-items");
-                for (var i = 0; i < x.length; i++) {
-                    if (elmnt != x[i] && elmnt != inp) {
-                        x[i].parentNode.removeChild(x[i]);
-                    }
-                }
-            }
-            /*execute a function when someone clicks in the document:*/
-            document.addEventListener("click", function(e) {
-                closeAllLists(e.target);
-            });
-        }
+        // "UUzX5VuQFrT49lV1Vvdup_xg"
     </script>
+
+
+
+
+
 
 </body>
 
